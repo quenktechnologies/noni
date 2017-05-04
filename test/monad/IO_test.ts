51cff19dab217e27d590ef3d2fad4a1da990591b
Lasana Murray <lmurray@quenk.com>
2017-05-04 09:58:07 -0400

    chore: clean up a bit

---
+++ test/monad/IO_test.ts
@@ -0,0 +1,29 @@
+import * as must from 'must/register';
+import { pure } from 'fpl/monad/IO';
+
+const x = 12;
+
+const mul2 = x => x * 2;
+
+const sqr = x => x * x;
+
+describe('IO', function() {
+
+    it('should obey the monad laws for return', function() {
+
+        let r = pure(x).chain(x => pure(mul2(x))).run();
+
+        must(r).be(mul2(x));
+
+    });
+
+    it('should obey the monad laws for bind', function() {
+
+        let r = pure(x).chain(x => pure(mul2(x))).chain(y => pure(sqr(y))).run();
+
+        must(r).be(pure(x).chain(x => pure(mul2(x)).chain(y => pure(sqr(y)))).run());
+
+    });
+
+});
+
