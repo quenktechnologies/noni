e121a13e5d150f73d194beadfe50f2792934546d
Lasana Murray <lmurray@quenk.com>
2018-10-10 11:20:39 -0400

    refactor(control/timer): remove arguments from tick
    Use a closure if you need to pass arguments.

--- src/control/timer.ts
+++ src/control/timer.ts
@@ -1,10 +1,9 @@
-import { Function } from '../data/function';
 
 /**
  * tick runs a function in the "next tick" using process.nextTick in node
  * or setTimeout(f, 0) elsewhere.
  */
-export const tick = <A, B>(f: Function<A, B>) => (window == null) ?
+export const tick = (f: ()=>void) => (window == null) ?
     setTimeout(f, 0) :
     process.nextTick(f)
 
