986e619d620c7c45e27529709ba7962ca19ced03
Lasana Murray <lmurray@quenk.com>
2018-10-15 16:32:46 -0400

    refactor(control/timer): add tick function

--- src/control/timer.ts
+++ src/control/timer.ts
@@ -3,7 +3,7 @@
  * tick runs a function in the "next tick" using process.nextTick in node
  * or setTimeout(f, 0) elsewhere.
  */
-export const tick = (f: ()=>void) => (window == null) ?
+export const tick = (f: () => void) => (typeof window == 'undefined') ?
     setTimeout(f, 0) :
-    process.nextTick(f)
+    process.nextTick(f);
 
