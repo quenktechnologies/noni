a7137f17fafe4fd51bf7d594b53a6e94034d4f83
Lasana Murray <lmurray@quenk.com>
2018-11-27 20:21:08 -0400

    chore: rebuild

--- lib/control/monad/future.d.ts
+++ lib/control/monad/future.d.ts
@@ -50,6 +50,11 @@ export declare abstract class Future<A> implements Monad<A> {
     catch(f: (e: Error) => Future<A>): Future<A>;
     finally(f: () => Future<A>): Future<A>;
     fork(onError: OnError, onSuccess: OnSuccess<A>): Compute<A>;
+    /**
+     * exec
+     * @private
+     */
+    abstract exec(c: Compute<A>): boolean;
 }
 /**
  * Pure constructor.
@@ -59,6 +64,46 @@ export declare class Pure<A> extends Future<A> {
     constructor(value: A);
     map<B>(f: (a: A) => B): Future<B>;
     ap<B>(ft: Future<(a: A) => B>): Future<B>;
+    exec(c: Compute<A>): boolean;
+}
+/**
+ * Bind constructor.
+ * @private
+ */
+export declare class Bind<A, B> extends Future<B> {
+    future: Future<A>;
+    func: (a: A) => Future<B>;
+    constructor(future: Future<A>, func: (a: A) => Future<B>);
+    exec(c: Compute<B>): boolean;
+}
+/**
+ * Step constructor.
+ * @private
+ */
+export declare class Step<A> extends Future<A> {
+    value: (a: A) => Future<A>;
+    constructor(value: (a: A) => Future<A>);
+    exec(c: Compute<A>): boolean;
+}
+/**
+ * Catch constructor.
+ * @private
+ */
+export declare class Catch<A> extends Future<A> {
+    future: Future<A>;
+    func: (e: Error) => Future<A>;
+    constructor(future: Future<A>, func: (e: Error) => Future<A>);
+    exec(c: Compute<A>): boolean;
+}
+/**
+ * Finally constructor.
+ * @private
+ */
+export declare class Finally<A> extends Future<A> {
+    future: Future<A>;
+    func: () => Future<A>;
+    constructor(future: Future<A>, func: () => Future<A>);
+    exec(c: Compute<A>): boolean;
 }
 /**
  * Raise constructor.
@@ -69,41 +114,7 @@ export declare class Raise<A> extends Future<A> {
     map<B>(_: (a: A) => B): Future<B>;
     ap<B>(_: Future<(a: A) => B>): Future<B>;
     chain<B>(_: (a: A) => Future<B>): Future<B>;
-}
-/**
- * Bind constructor.
- * @private
- */
-export declare class Bind<A, B> extends Future<B> {
-    future: Future<A>;
-    func: (a: A) => Future<B>;
-    constructor(future: Future<A>, func: (a: A) => Future<B>);
-}
-/**
- * Step constructor.
- * @private
- */
-export declare class Step<A> extends Future<A> {
-    value: (a: A) => Future<A>;
-    constructor(value: (a: A) => Future<A>);
-}
-/**
- * Catch constructor.
- * @private
- */
-export declare class Catch<A> extends Future<A> {
-    future: Future<A>;
-    func: (e: Error) => Future<A>;
-    constructor(future: Future<A>, func: (e: Error) => Future<A>);
-}
-/**
- * Finally constructor.
- * @private
- */
-export declare class Finally<A> extends Future<A> {
-    future: Future<A>;
-    func: () => Future<A>;
-    constructor(future: Future<A>, func: () => Future<A>);
+    exec(c: Compute<A>): boolean;
 }
 /**
  * Run constructor.
@@ -112,6 +123,7 @@ export declare class Finally<A> extends Future<A> {
 export declare class Run<A> extends Future<A> {
     value: Job<A>;
     constructor(value: Job<A>);
+    exec(c: Compute<A>): boolean;
 }
 /**
  * Supervisor for Computations.
@@ -169,9 +181,6 @@ export declare class Compute<A> implements Supervisor<A> {
      * and invoke the canceller for the currently executing Future.
      */
     abort(): void;
-    /**
-     * run this Compute.
-     */
     run(): void;
 }
 /**
@@ -207,10 +216,21 @@ export declare const fromAbortable: <A>(abort: Aborter) => (f: CallbackReceiver<
  */
 export declare const fromCallback: <A>(f: CallbackReceiver<A>) => Future<A>;
 /**
+ * parallelN runs a list of batched Futures one batch at a time.
+ */
+export declare const parallelN: <A>(list: Future<A>[][]) => Future<A[][]>;
+/**
  * parallel runs a list of Futures in parallel failing if any
  * fail and succeeding with a list of successful values.
  */
 export declare const parallel: <A>(list: Future<A>[]) => Future<A[]>;
+/**
+ * sequential execution of a list of futures.
+ *
+ * This function succeeds with a list of all results or fails on the first
+ * error.
+ */
+export declare const sequential: <A>(list: Future<A>[]) => Future<A[]>;
 /**
  * race given a list of Futures, will return a Future that is settled by
  * the first error or success to occur.
