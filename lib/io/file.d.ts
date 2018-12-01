4b9aa04423aa3cd4806feafae3414a56cd876e55
Lasana Murray <lmurray@quenk.com>
2018-11-30 20:50:46 -0400

    chore: rebuild

--- lib/io/file.d.ts
+++ lib/io/file.d.ts
@@ -1,9 +1,51 @@
+/// <reference types="node" />
+import * as fs from 'fs';
 import { Future } from '../control/monad/future';
+import { Maybe } from '../data/maybe';
+import { Either } from '../data/either';
+declare const Stats: typeof fs.Stats;
+export { Stats };
 /**
  * Path to a file.
  */
 export declare type Path = string;
 /**
- * readText reads the contents of a file as a utf8 encoded text file.
+ * Contents of a file.
  */
-export declare const readText: (path: string) => Future<string>;
+export declare type Contents = string | Buffer | NodeJS.TypedArray | DataView;
+/**
+ * stat (safe) wrapper.
+ */
+export declare const stat: (path: string) => Future<fs.Stats>;
+/**
+ * exists (safe) wrapper.
+ */
+export declare const exists: (path: string) => Future<Maybe<string>>;
+/**
+ * isDirectory (safe) wrapper.
+ */
+export declare const isDirectory: (path: string) => Future<Either<boolean, boolean>>;
+/**
+ * isFile (safe) wrapper.
+ */
+export declare const isFile: (path: string) => Future<Either<boolean, boolean>>;
+/**
+ * readdir (safe) wrapper
+ */
+export declare const readdir: (path: string) => Future<{}>;
+/**
+ * readFile (safe) wrapper
+ */
+export declare const readFile: (path: string, options: string | object) => Future<Contents>;
+/**
+ * readTextFile reads the contents of a file as a utf8 encoded text file.
+ */
+export declare const readTextFile: (path: string) => Future<string>;
+/**
+ * writeFile (safe) wrapper.
+ */
+export declare const writeFile: (path: string, contents: Contents, options: string | object) => Future<Contents>;
+/**
+ * writeTextFile writes the passed contents to a a file location.
+ */
+export declare const writeTextFile: (path: string, contents: string) => Future<Contents>;
--- lib/io/file.ts
+++ lib/io/file.ts
@@ -1,5 +1,11 @@
 import * as fs from 'fs';
-import { Future, fromCallback } from '../control/monad/future';
+import { Future, pure, fromCallback } from '../control/monad/future';
+import { Maybe, just, nothing } from '../data/maybe';
+import { Either, fromBoolean } from '../data/either';
+
+const Stats = fs.Stats;
+
+export { Stats };
 
 /**
  * Path to a file.
@@ -7,7 +13,71 @@ import { Future, fromCallback } from '../control/monad/future';
 export type Path = string;
 
 /**
- * readText reads the contents of a file as a utf8 encoded text file.
+ * Contents of a file.
  */
-export const readText = (path: Path): Future<string> =>
-    fromCallback(cb => fs.readFile(path, { encoding: 'utf8' }, cb));
+export type Contents
+    = string
+    | Buffer
+    | NodeJS.TypedArray
+    | DataView
+    ;
+
+/**
+ * stat (safe) wrapper.
+ */
+export const stat = (path: Path): Future<fs.Stats> =>
+    fromCallback(cb => fs.stat(path, cb));
+
+/**
+ * exists (safe) wrapper.
+ */
+export const exists = (path: Path): Future<Maybe<Path>> =>
+    stat(path)
+        .map(() => just(path))
+        .catch(() => pure(nothing()));
+
+/**
+ * isDirectory (safe) wrapper.
+ */
+export const isDirectory = (path: Path): Future<Either<boolean, boolean>> =>
+    stat(path)
+        .map(s => fromBoolean(s.isDirectory()))
+
+/**
+ * isFile (safe) wrapper.
+ */
+export const isFile = (path: Path): Future<Either<boolean, boolean>> =>
+    stat(path)
+        .map(s => fromBoolean(s.isFile()));
+
+/**
+ * readdir (safe) wrapper
+ */
+export const readdir = (path: Path) =>
+    fromCallback(cb => fs.readdir(path, cb));
+
+/**
+ * readFile (safe) wrapper
+ */
+export const readFile = (path: Path, options: string | object): Future<Contents> =>
+    fromCallback(cb => fs.readFile(path, options, cb));
+
+/**
+ * readTextFile reads the contents of a file as a utf8 encoded text file.
+ */
+export const readTextFile = (path: Path): Future<string> =>
+    <Future<string>>readFile(path, 'utf8');
+
+/**
+ * writeFile (safe) wrapper.
+ */
+export const writeFile = (path: Path, contents: Contents, options: string | object)
+    : Future<Contents> =>
+    fromCallback(cb => fs.writeFile(path, contents, options, cb));
+
+/**
+ * writeTextFile writes the passed contents to a a file location.
+ */
+export const writeTextFile = (path: Path, contents: string) =>
+    writeFile(path, contents, 'utf8');
+
