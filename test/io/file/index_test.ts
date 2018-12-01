6e10d0629579f8d1248fa32841f9e5015c800a43
Lasana Murray <lmurray@quenk.com>
2018-11-30 20:50:16 -0400

    refactor(io/file): add more file functions

--- test/io/file/index_test.ts
+++ test/io/file/index_test.ts
@@ -1,16 +1,25 @@
 import { must } from '@quenk/must';
 import { toPromise } from '../../../src/control/monad/future';
-import { readText } from '../../../src/io/file';
+import { readTextFile, writeTextFile } from '../../../src/io/file';
 
 const ABOUT = 'This is a flag🇹.\n';
-
+const ABOUT_FILE = `${__dirname}/fixtures/about`;
 
 describe('file', () => {
 
-    describe('readText', () => {
+    describe('readTextFile', () => {
 
         it('must read a file\'s contents as utf8', () =>
-            toPromise(readText(`${__dirname}/fixtures/about`)
+            toPromise(readTextFile(ABOUT_FILE)
+                .map(contents => must(contents).equal(ABOUT))));
+
+    });
+
+    describe('writeTextFile', () => {
+
+        it('must write a file\'s contents as utf8', () =>
+            toPromise(writeTextFile(ABOUT_FILE, ABOUT)
+                .chain(() => readTextFile(ABOUT_FILE))
                 .map(contents => must(contents).equal(ABOUT))));
 
     });
