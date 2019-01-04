04394f5d4f10e35a894de8d28a8ecfcd47cd5e6e
Lasana Murray <lmurray@quenk.com>
2019-01-04 14:44:42 -0400

    refactor(data/type): add constructor type

---
+++ lib/data/type/constructor.d.ts
@@ -0,0 +1,10 @@
+import { Type } from './';
+/**
+ * Constructor interface.
+ *
+ * This definition exists for use in cases where we need to indicate
+ * a type is a constructor but are not concerned with its parameters.
+ */
+export interface Constructor<T> {
+    new (...args: Type[]): T;
+}
