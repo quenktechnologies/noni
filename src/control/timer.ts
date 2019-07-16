19b534b2d9747f4fd711b09f718b629f3137dc18
Lasana Murray <lmurray@quenk.com>
2019-07-16 11:50:21 -0400

    refactor(control/timer): add debounce and throttle funcs

--- src/control/timer.ts
+++ src/control/timer.ts
@@ -1,3 +1,5 @@
+import { Milliseconds } from './time';
+import { Function } from '../data/function';
 
 /**
  * tick runs a function in the "next tick" using process.nextTick in node
@@ -7,3 +9,62 @@ export const tick = (f: () => void) => (typeof window == 'undefined') ?
     setTimeout(f, 0) :
     process.nextTick(f);
 
+
+/**
+ * debounce delays the application of a function until the specified time
+ * has passed.
+ *
+ * If multiple attempts to apply the function have occured, then each attempt
+ * will restart the delay process. The function will only ever be applied once
+ * after the delay, using the value of the final attempt for application.
+ */
+export const debounce =
+    <A>(f: Function<A, void>, delay: Milliseconds): Function<A, void> => {
+
+        let id: number = -1;
+
+        return (a: A) => {
+
+            if (id === -1) {
+
+                id = <number><unknown>setTimeout(() => f(a), delay);
+
+            } else {
+
+                clearTimeout(id);
+
+                id = <number><unknown>setTimeout(() => f(a), delay);
+
+            }
+
+        }
+
+    }
+
+/**
+ * throttle limits the application of a function to occur only one within the
+ * specified duration.
+ *
+ * The first application will execute immediately subsequent applications
+ * will be ignored until the duration has passed.
+ */
+export const throttle =
+    <A>(f: Function<A, void>, duration: Milliseconds): Function<A, void> => {
+
+        let wait: boolean = false;
+
+        return (a: A) => {
+
+            if (wait === false) {
+
+                f(a);
+
+                wait = true;
+
+                setTimeout(() => wait = false, duration);
+
+            }
+
+        }
+
+    }
---
+++ test/control/timer_test.ts
@@ -0,0 +1,73 @@
+import { assert } from '@quenk/test/lib/assert';
+import { debounce, throttle } from '../../src/control/timer';
+
+describe('timer', () => {
+
+    describe('debounce', () => {
+
+        it('should only execute the function once', done => {
+
+            let sum = 0;
+            let f = debounce((n: number) => sum = sum + n, 500);
+
+            f(1);
+            f(1);
+            f(1);
+
+            setTimeout(() => f(1), 1500);
+
+            setTimeout(() => {
+
+                try {
+
+                    assert(sum).equal(2);
+                    done(null);
+
+                } catch (e) {
+
+                    done(e);
+
+                }
+
+            }, 2500)
+
+        });
+
+    });
+
+    describe('throttle', () => {
+
+        it('should limit application', done => {
+
+            let sum = 0;
+
+            let f = throttle((n: number) => sum = sum + n, 500);
+
+            f(1);
+            f(1);
+            f(1);
+            setTimeout(() => f(1), 100);
+            setTimeout(() => f(1), 200);
+            setTimeout(() => f(1), 300);
+            setTimeout(() => f(1), 600);
+
+            setTimeout(() => {
+
+                try {
+
+                    assert(sum).equal(2);
+                    done(null);
+
+                } catch (e) {
+
+                    done(e);
+
+                }
+
+            }, 1000);
+
+        });
+
+    });
+
+});
