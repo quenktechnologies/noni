9ed87c77039c44860a9a5b68615ea3fc503690ba
Lasana Murray <lmurray@quenk.com>
2019-12-04 14:03:41 -0400

    refactor(math): add round function

--- lib/math.d.ts
+++ lib/math.d.ts
@@ -7,6 +7,43 @@
  */
 export declare type Integer = number;
 /**
+ * PositiveInteger is an Integer that is > 0.
+ */
+export declare type PositiveInteger = number;
+/**
+ * NegativeInteger is an Integer that is < 0.
+ */
+export declare type NegativeInteger = number;
+/**
  * isMultipleOf tests whether the Integer 'y' is a multiple of x.
  */
 export declare const isMultipleOf: (x: number, y: number) => boolean;
+/**
+ * round a number "x" to "n" places (n defaults to 0 places).
+ *
+ * This uses the Math.round(x * n) / n method however we take into
+ * consideration the Math.round(1.005 * 100) / 100 === 1 issue by use of an
+ * offset:
+ *
+ * sign * (round((abs(x) * 10^n) + (1 / 10^n+1)) / 10^n)
+ *
+ * Where:
+ *
+ * sign is the sign of x
+ * round is Math.round
+ * abs is Math.abs
+ * (1 / 10^n+1) is the offset.
+ *
+ * The offset is only used if n is more than zero. The absolute value of x
+ * is used in the calculation to avoid JavaScript idiosyncracies when rounding
+ * 0.5:
+ * (Math.round((1.005 * 100)+0.001) / 100) === 1.01
+ *
+ * whereas
+ * (Math.round((-1.005 * 100)+0.001) / 100) === -1
+ *
+ * See the description [here]( https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round)
+ * for more details.
+ *
+ */
+export declare const round: (x: number, n?: number) => number;
