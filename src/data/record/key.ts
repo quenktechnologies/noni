1ab831bc8e5e5763521bda8965d47c97c56effe3
Lasana Murray <lmurray@quenk.com>
2020-08-08 19:34:58 -0400

    refactor: moves some record functions around close #29

---
+++ src/data/record/key.ts
@@ -0,0 +1,71 @@
+/**
+ * This module provides functions for operating on Records based on their
+ * keys.
+ */
+
+/** imports */
+import { Record, reduce, set } from './';
+
+/**
+ * MapFunc used by mapKeys
+ */
+export type MapFunc<A>
+    = (key: string, value: A, rec: Record<A>) => string
+    ;
+
+/**
+ * map maps over the property names of a Record producing a new Record
+ * with its keys produced by the MapFunc provided.
+ */
+export const map = <A>(rec: Record<A>, f: MapFunc<A>): Record<A> =>
+    reduce(rec, <Record<A>>{}, (p, c, k) => {
+
+        p = set(p, f(k, c, rec), c);
+        return p;
+
+    });
+
+/**
+ * intersect set operation.
+ *
+ * Produces a new Record containing only the properties of the keys
+ * that intersect the two records. The value of the properties are sourced
+ * from the left Record.
+ */
+export const intersect = <A, B>(left: Record<A>, right: Record<B>): Record<A> =>
+    reduce(left, <Record<A>>{}, (p, c, k) => {
+
+        if (right.hasOwnProperty(k))
+            p = set(p, k, c);
+
+        return p;
+
+    });
+
+/**
+ * difference set operation.
+ *
+ * Produces a new Record containing the propertiesof the left Record less
+ * any keys appearing in the right one.
+ */
+export const difference = 
+  <A, B>(left: Record<A>, right: Record<B>): Record<A> =>
+    reduce(left, <Record<A>>{}, (p, c, k) => {
+
+        if (!right.hasOwnProperty(k))
+            p = set(p, k, c);
+
+        return p;
+
+    });
+
+/**
+ * project a Record according to the field specification given.
+ *
+ * This does not treat the keys of the spec object as paths.
+ * For that, use the project function from the path submodule.
+ */
+export const project =
+    <A>(spec: Record<boolean>, rec: Record<A>): Record<A> =>
+        reduce(spec, <Record<A>>{}, (p, c, k) =>
+            (c === true) ? set(p, k, rec[k]) : p);
--- test/data/record/path_test.ts
+++ test/data/record/path_test.ts
@@ -12,9 +12,6 @@ import {
     flatten,
     unflatten,
     unescapeRecord,
-    intersect,
-    difference,
-    map,
     project
 } from '../../../src/data/record/path';
 import { Record } from '../../../src/data/record';
@@ -621,81 +618,6 @@ describe('path', () => {
         });
     })
 
-    describe('intersect', () => {
-
-        it('should retain the left', () => {
-
-            let l: Record<number> = { a: 1, b: 2, c: 3 };
-            let r: Record<number> = { e: 4, b: 5, c: 6 };
-
-            assert(intersect(l, r)).equate({ b: 2, c: 3 });
-
-        });
-
-        it('should not overwrite Object#prototype', () => {
-
-            let rec1 = JSON.parse(`{
-                "__proto__": { "admin": true }
-            }`);
-
-            let rec2 = JSON.parse(`{ "__proto__": { } }`);
-            let result = intersect(rec1, rec2);
-
-            assert(result.admin).undefined();
-
-        });
-
-    });
-
-    describe('difference', () => {
-
-        it('should retain the ones without matches only', () => {
-
-            let l: Record<number> = { a: 1, b: 2, c: 3 };
-            let r: Record<number> = { e: 4, b: 5, c: 6 };
-
-            assert(difference(l, r)).equate({ a: 1 });
-
-        });
-
-        it('should not overwrite Object#prototype', () => {
-
-            let rec = JSON.parse(`{
-                "__proto__": { "admin": true }
-            }`);
-
-            let result = difference(rec, {});
-
-            assert(result.admin).undefined();
-
-        });
-
-    });
-
-    describe('map', () => {
-
-        it('should work', () => {
-
-            let o: Record<number> = { a: 1, b: 2, c: 3 };
-
-            assert(map(o, p => `n${p}`)).equate({ na: 1, nb: 2, nc: 3 });
-
-        });
-
-        it('should not overwrite Object#prototype', () => {
-
-            let rec = JSON.parse(`{
-             "__proto__": { "admin": true } 
-            }`);
-
-            let result = map(rec, p => p);
-
-            assert(result.admin).undefined();
-
-        })
-
-    });
-
     describe('project', () => {
 
         const src = { a: 1, b: 2, c: [3], d: { a: 1, b: 2, c: 3 } };
@@ -744,5 +666,6 @@ describe('path', () => {
         });
 
     });
+
 
 });
