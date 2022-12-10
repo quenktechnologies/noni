5efdeaeb34779e68b490e4c9fd5c8a4a26a81a56
Lasana Murray <lmurray@quenk.com>
2022-12-09 21:30:56 -0400

    close #68: add exec and execFile wrappers

---
+++ test/platform/node/cli/err.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for 1..2; do
---
+++ test/platform/node/cli/index_test.ts
@@ -0,0 +1,148 @@
+import { assert } from '@quenk/test/lib/assert';
+
+import { doFuture, voidPure, raise } from '../../../../lib/control/monad/future';
+import { exec, execFile } from '../../../../lib/platform/node/cli';
+
+describe('cli', () => {
+
+    describe('exec', () => {
+
+        it('should return stdout', () => doFuture(function*() {
+
+            let [stdout, stderr] = yield exec(`${__dirname}/stdout.sh`);
+
+            assert(stderr).equal('');
+
+            assert(stdout).equal('test\n');
+
+            return voidPure;
+
+        }))
+
+        it('should return stderr', () => doFuture(function*() {
+
+            let [stdout, stderr] = yield exec(`${__dirname}/stderr.sh`);
+
+            assert(stdout).equal('');
+
+            assert(stderr).equal('test\n');
+
+            return voidPure;
+
+        }))
+
+        it('should raise when the script encounters an error', () =>
+            doFuture(function*() {
+
+                let called = false;
+
+                yield exec(`${__dirname}/err.sh`).catch(e => {
+
+                    called = true;
+
+                    return e.message.includes('syntax error') ?
+                        voidPure :
+                        raise(e)
+
+                });
+
+                assert(called).true();
+
+                return voidPure;
+
+            }))
+
+        it('should raise when the script does not exist', () =>
+            doFuture(function*() {
+
+                let called = false;
+
+                yield exec(`${__dirname}/nobody.sh`).catch(e => {
+
+                    called = true;
+
+                    return e.message.includes('not found') ?
+                        voidPure :
+                        raise(e)
+
+                });
+
+                assert(called).true();
+
+                return voidPure;
+
+            }))
+
+    })
+
+    describe('execFile', () => {
+
+        it('should return stdout', () => doFuture(function*() {
+
+            let [stdout, stderr] = yield execFile(`${__dirname}/stdout.sh`);
+
+            assert(stderr).equal('');
+
+            assert(stdout).equal('test\n');
+
+            return voidPure;
+
+        }))
+
+        it('should return stderr', () => doFuture(function*() {
+
+            let [stdout, stderr] = yield execFile(`${__dirname}/stderr.sh`);
+
+            assert(stdout).equal('');
+
+            assert(stderr).equal('test\n');
+
+            return voidPure;
+
+        }))
+
+        it('should raise when the script encounters an error', () =>
+            doFuture(function*() {
+
+                let called = false;
+
+                yield execFile(`${__dirname}/err.sh`).catch(e => {
+
+                    called = true;
+
+                    return e.message.includes('syntax error') ?
+                        voidPure :
+                        raise(e)
+
+                });
+
+                assert(called).true();
+
+                return voidPure;
+
+            }))
+
+        it('should raise when the script does not exist', () =>
+            doFuture(function*() {
+
+                let called = false;
+
+                yield execFile(`${__dirname}/nobody.sh`).catch(e => {
+
+                    called = true;
+
+                    return e.message.includes('ENOENT') ?
+                        voidPure :
+                        raise(e)
+
+                });
+
+                assert(called).true();
+
+                return voidPure;
+
+            }))
+
+    })
+
+})
