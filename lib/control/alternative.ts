6e201477f469b2aafee64336cd6bd46eadd0375b
Lasana Murray <lmurray@quenk.com>
2023-03-03 23:07:31 -0400

    build: stop checking in lib

--- lib/control/alternative.ts
+++
@@ -1,7 +0,0 @@
-import {Applicative} from './applicative';
-import {Plus} from './plus';
-
-/**
- * Alternative
- */
-export interface Alternative<A> extends Applicative<A>, Plus<A> {}
--- lib/crypto/uuid.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * generateV4 generates a string based on the 4th version of the RFC.
- */
-export declare const generateV4: (noDash?: boolean) => string;
-/**
- * @private
- */
-export declare const webGenerateV4: (noDash?: boolean) => string;
