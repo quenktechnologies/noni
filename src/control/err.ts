bf73bed6b39e04883b87c88dcb7e78d6e99ff6ea
Lasana Murray <lmurray@quenk.com>
2023-04-02 13:03:42 -0400

    close #91: add raise to except

---
+++ src/control/err.ts
@@ -0,0 +1,65 @@
+import { isString } from "../data/type";
+
+/**
+ * ErrorMessage is a string indicating the nature of an error.
+ */
+export type ErrorMessage = string;
+
+/**
+ * ErrorStack is string where each line is an entry into a stack trace.
+ */
+export type ErrorStack = string;
+
+/**
+ * Err describes the JS Error interface independent
+ * of the default Error machinery.
+ * 
+ * This is done to avoid hacks required when extending the Error constructor.
+ */
+export interface Err {
+
+    /**
+     * message describing the error that occurred.
+     */
+    message: ErrorMessage
+
+    /**
+     * stack contains the call stack on platforms where available.
+     */
+    stack?: ErrorStack
+
+}
+
+/** 
+ * toError converts a string or an Err to an error.
+ */
+export const toError = (e: ErrorMessage | Err | Error): Error => {
+    if (isString(e))
+        return new Error(e);
+    else if (!(e instanceof Error))
+        return new Error(e ? e.message ? e.message : undefined : undefined);
+    return e;
+}
+
+// @deprecated
+export { toError as convert }
+
+/**
+ * raise the supplied Error.
+ *
+ * This function exists to maintain a functional style in situations where 
+ * you may actually want to throw an error.
+ */
+export const raise = (e: Err) => {
+
+    if (e instanceof Error) {
+
+        throw e;
+
+    } else {
+
+        throw new Error(e.message);
+
+    }
+
+}
---
+++ src/control/except.ts
@@ -0,0 +1,34 @@
+import { Either, left, right } from '../data/either';
+import { Err, ErrorMessage, toError } from './err';
+
+/**
+ * Except type represents a value that may fail with an error.
+ *
+ * It is a version of Either specialized with an Err type on the left-hand side.
+ */
+export type Except<A> = Either<Err, A>;
+
+/**
+ * raise an Except.
+ *
+ * This promotes a value to an Error wrapped inside a left.
+ */
+export const raise = <A>(msg: ErrorMessage | Err | Error): Except<A> =>
+    left(toError(msg));
+
+/**
+ * attempt a synchronous computation that may throw an exception.
+ */
+export const attempt = <A>(f: () => A): Except<A> => {
+
+    try {
+
+        return right(f());
+
+    } catch (e) {
+
+        return <Except<A>>left(e);
+
+    }
+
+}
